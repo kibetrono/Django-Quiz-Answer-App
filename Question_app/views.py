e51341c7158512a34ffc993d7cd4bae12c7b5dd4
email <kibetdavidro@gmail.com>
2022-03-23 12:01:38 +0300

    add the updateprofile view

--- Question_app/views.py
+++ Question_app/views.py
@@ -20,3 +20,43 @@ def my_profile(request):
     context={'profile': profile}
     return render(request, 'Question_app/profile.html', context)
 
+@login_required(login_url='/accounts/login/')
+def update_profile_form(request):
+
+    context={}
+    return render(request, 'Question_app/updateProfile.html',context)
+
+@login_required(login_url='/accounts/login/')
+def update_profile(request):
+    if request.method == "POST":
+        current_user = request.user
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        username = request.POST["username"]
+        email = request.POST["email"]
+        bio = request.POST["bio"]
+        name = request.POST["first_name"] + " " + request.POST["last_name"]
+
+        profile_image = request.FILES["profile_pic"]
+        profile_image = cloudinary.uploader.upload(profile_image)
+        profile_url = profile_image["url"]
+        user = User.objects.get(id=current_user.id)
+        if Profile.objects.filter(user_id=current_user.id).exists():
+
+            profile = Profile.objects.get(user_id=current_user.id)
+            profile.profile_pic = profile_url
+            profile.save()
+        else:
+            profile = Profile(user_id=current_user.id,name=name,profile_pic=profile_url)
+
+            profile.save_profile()
+
+        user.first_name = first_name
+        user.last_name = last_name
+        user.username = username
+        user.email = email
+        user.bio=bio
+        user.save()
+        return redirect("/profile")
+    else:
+        return render(request, "Question_app/profile.html")
